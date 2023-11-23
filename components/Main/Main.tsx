342ec73a7d59c627a90830bc5ab2a585cebfc585
Lao <email@1657098590.com>
2023-11-23 12:47:39 +0800

    上线版1

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -12,6 +12,14 @@ import Specialdetail from '../Main/Specialdetail'
 import Researchdetail from '../Main/Researchdetail'
 import { SheetDataInfo, exportExcelFile } from '@/lib/utils/xlsx'
 
+export interface DataType {
+  [key: string]: string | number
+}
+interface DListType {
+  datalist: DataType[]
+  sheetName: string[]
+}
+
 const { Header, Content, Footer, Sider } = Layout
 const { Option } = Select
 
@@ -23,19 +31,48 @@ const items: MenuProps['items'] = arr.map((_, index) => ({
   title: arr[index],
 }))
 
+const initialdata: DListType = {
+  datalist: [], // 你的 DataType[] 类型的数据
+  sheetName: [''],
+}
+
 const Main = () => {
   const [kind, setKind] = useState<string>('1')
-
+  const [dataList, setDataList] = useState<DListType>(initialdata)
+  dataList.sheetName[0] = '宝宝资料'
+  dataList.sheetName[1] = '母亲资料'
+  dataList.sheetName[2] = '父亲资料'
+  dataList.sheetName[3] = '主要看护者资料'
+  dataList.sheetName[4] = '特殊资料'
+  dataList.sheetName[5] = '研究资料(早产儿按纠正年龄算)'
   const exportExcel = () => {
-    const data: SheetDataInfo = {
-      sheetName: 'test',
-      data: [{ 姓名: '123', 年龄: '43252', 测试: '啊沙发沙发' }],
+    const data: SheetDataInfo[] = []
+    for (let i = 0; i < 6; i++) {
+      data[i] = { data: [], sheetName: dataList.sheetName[i] }
+      if (dataList.datalist[i]) {
+        data[i].data = [dataList.datalist[i]]
+      }
     }
-    exportExcelFile([data])
+    exportExcelFile(data)
+    // exportExcelFile([
+    //   {
+    //     data: [{ '1': '2' }],
+    //     sheetName: '123',
+    //     opts: {},
+    //   },
+    // ])
   }
 
+  const saveData = (index: number) => {
+    return (data: DataType) => {
+      dataList.datalist[index] = data
+    }
+  }
+
+  console.log(dataList)
+
   return (
-    <Layout className='w-full h-full'>
+    <Layout className='w-full h-full' style={{ height: '900px' }}>
       <Header className='flex justify-end items-center h-[64px]'>
         <Button onClick={exportExcel}>导出</Button>
       </Header>
@@ -53,17 +90,17 @@ const Main = () => {
         </Sider>
         <Content className='h-full box-border p-6'>
           {kind === '1' ? (
-            <Baobao />
+            <Baobao save={saveData(0)} />
           ) : kind === '2' ? (
-            <Mom />
+            <Mom save={saveData(1)} />
           ) : kind === '3' ? (
-            <Father />
+            <Father save={saveData(2)} />
           ) : kind === '4' ? (
-            <Caregiver />
+            <Caregiver save={saveData(3)} />
           ) : kind === '5' ? (
-            <Specialdetail />
+            <Specialdetail save={saveData(4)} />
           ) : (
-            <Researchdetail />
+            <Researchdetail save={saveData(5)} />
           )}
         </Content>
       </Layout>
